d998112f6f1fb1c7799b7d20df8e64d859d0c2ba
jasmith188 <jasmith.188@gmail.com>
2023-07-16 20:43:02 -0400

    update carousel

--- src/Carousel.js
+++ src/Carousel.js
@@ -9,7 +9,7 @@ export const Carousel = () => {
           <div class="mx-auto max-w-2xl lg:mx-0">
             <h2 class="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
               From the blog
-            </h2
+            </h2>
             <p class="mt-2 text-lg leading-8 text-gray-600">
               Learn how to grow your business with our expert advice.
             </p>
